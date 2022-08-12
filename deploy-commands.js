8db3e1edd02725cdd9d950cedc07e0ee7c30554a
Lazar Gosic <lazar.gosic@gmail.com>
2022-08-12 20:23:47 +0200

    moved configuration to env file

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,8 @@
 const { SlashCommandBuilder, Routes } = require('discord.js');
 const { REST } = require('@discordjs/rest');
-const { clientId, guildId, token } = require('./config.json');
+
+require('dotenv').config();
+//const { clientId, guildId, token } = require('./config.json');
 
 const commands = [
     new SlashCommandBuilder()
@@ -14,8 +16,8 @@ const commands = [
 ]
     .map(command => command.toJSON());
 
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
     .then(() => console.log('Successfully registered application commands.'))
     .catch(console.error);
