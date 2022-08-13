f1c194a57c0b96dfb4ef91f558be1702f3339fda
Lazar Gosic <lazar.gosic@gmail.com>
2022-08-13 03:12:29 +0200

    fixed issue with ignore lands functionality

--- index.js
+++ index.js
@@ -26,10 +26,13 @@ client.on('interactionCreate', async interaction => {
     await interaction.deferReply();
 
     const decklist = interaction.options.getString('decklist').split('1');
-    const ignorelands = interaction.options.getString('ignorelands');
+    var ignorelands = interaction.options.getString('ignorelands');
 
     if (commandName === 'mana') {
-        if (ignorelands == null || ignorelands == undefined) ignorelands = "X";
+        if (ignorelands == null) {
+            ignorelands = 'La La Land';
+        }
+
         const remoteUrl = 'https://rainbowcalculator.herokuapp.com/api/manabase/' + decklist.join('|') + '/' + ignorelands;
         const manabaseRequest = await request(remoteUrl);
         interaction.editReply('sending request to ' + remoteUrl);
@@ -38,30 +41,30 @@ client.on('interactionCreate', async interaction => {
         var message = "";
 
         if (res.cardsNotFound.length > 0) {
-            message += '\n I couldn\'t find these cards: ' + res.cardsNotFound;
+            message += '\nI couldn\'t find these cards: ' + res.cardsNotFound;
         }
 
         if (res.excludedCards.length > 0) {
-            message += '\n Ignored the following cards due to difficult calculation: ' + res.excludedCards;
+            message += '\nIgnored the following cards due to difficult calculation: ' + res.excludedCards;
         }
 
         var subtract = 0;
         if (res.removedLands.length > 0) {
-            message += '\n Did NOT take into account these lands: ' + res.removedLands;
+            message += '\nDid NOT take into account these lands: ' + res.removedLands;
             subtract = res.removedLands.length;
         }
 
         for (var i = 0; i < res.colorRequirements.length; i++) {
             var r = res.colorRequirements[i];
             if (r.amount == 0) continue;
-            message += '\n Need ' + r.amount + ' sources of {' + r.color + '}';
+            message += '\nNeed ' + r.amount + ' sources of {' + r.color + '}';
         }
 
         var cardcount = decklist.length - subtract;
         var sources = res.lands.length;
         var total = cardcount + sources;
-        if (total < 100) message += '\n By the way, your deck could use some more cards or a higher curve!';
-        if (sources > total) message += '\n By the way, there are too many cards now for the required card count and/or mana curve, try adjusting your deck!';
+        if (total < 100) message += '\nBy the way, your deck could use some more cards or a higher curve!';
+        if (sources > total) message += '\nBy the way, there are too many cards now for the required card count and/or mana curve, try adjusting your deck!';
 
         message += '\nTotal card count: ' + total;
 
