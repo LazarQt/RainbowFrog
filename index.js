08b340206ebf1a573f50ecb02743d3f2732aa385
Lazar Gosic <lazar.gosic@gmail.com>
2022-08-13 02:55:03 +0200

    fixed ignore lands

--- index.js
+++ index.js
@@ -29,38 +29,39 @@ client.on('interactionCreate', async interaction => {
     const ignorelands = interaction.options.getString('ignorelands');
 
     if (commandName === 'mana') {
+        if (ignorelands == null || ignorelands == undefined) ignorelands = "X";
         const remoteUrl = 'https://rainbowcalculator.herokuapp.com/api/manabase/' + decklist.join('|') + '/' + ignorelands;
         const manabaseRequest = await request(remoteUrl);
         interaction.editReply('sending request to ' + remoteUrl);
-        const res = await getJSONResponse(manabaseRequest.body);     
-        
+        const res = await getJSONResponse(manabaseRequest.body);
+
         var message = "";
 
-        if(res.cardsNotFound.length > 0) {
+        if (res.cardsNotFound.length > 0) {
             message += '\n I couldn\'t find these cards: ' + res.cardsNotFound;
         }
 
-        if(res.excludedCards.length > 0) {
+        if (res.excludedCards.length > 0) {
             message += '\n Ignored the following cards due to difficult calculation: ' + res.excludedCards;
         }
 
         var subtract = 0;
-        if(res.removedLands.length > 0) {
+        if (res.removedLands.length > 0) {
             message += '\n Did NOT take into account these lands: ' + res.removedLands;
             subtract = res.removedLands.length;
         }
 
-        for(var i = 0; i < res.colorRequirements.length; i++) {
+        for (var i = 0; i < res.colorRequirements.length; i++) {
             var r = res.colorRequirements[i];
-            if(r.amount == 0) continue;
+            if (r.amount == 0) continue;
             message += '\n Need ' + r.amount + ' sources of {' + r.color + '}';
         }
-        
+
         var cardcount = decklist.length - subtract;
         var sources = res.lands.length;
         var total = cardcount + sources;
-        if(total < 100) message += '\n By the way, your deck could use some more cards or a higher curve!';
-        if(sources > total) message += '\n By the way, there are too many cards now for the required card count and/or mana curve, try adjusting your deck!';
+        if (total < 100) message += '\n By the way, your deck could use some more cards or a higher curve!';
+        if (sources > total) message += '\n By the way, there are too many cards now for the required card count and/or mana curve, try adjusting your deck!';
 
         message += '\nTotal card count: ' + total;
 
